54c03da76812ebea5784b1f37002fffaccfbb1bc
MomentarilyDisabled <70651449+MomentarilyDisabled@users.noreply.github.com>
2023-04-17 11:50:30 -0500

    Update nuke.js

--- commands/nuke.js
+++ commands/nuke.js
@@ -9,21 +9,27 @@ module.exports = {
 	async execute(message) {
         const args = message.content.slice(prefix.length).trim().split(" ");
 	    const command = args.shift().toLowerCase();
+        const pn = args[1] // pn = "ping number" or "number of pings"
         
-
+        if (args[1]) { 
+            if(isNaN(args[1])) { // is [# of pings] argument NaN, which also deals with empty string
+                pn = 20
+            }
+        }
+        
         if (args[0]) {
             const user = getUserFromMention(args[0]);
             if(!user) {
                 return message.reply('Who?')
             }
 
-            for (i = 0; i < args[1]; i++) {
+            for (i = 0; i < pn; i++) {
                 message.channel.send(`${user} is being nuked!`);
             }
             return message.channel.send(`${user} has been nuked!`);
         }
 
-        for (i = 0; i < args[1]; i++) {
+        for (i = 0; i < pn; i++) {
                 message.channel.send(`${message.author} is being nuked!`);
             }
         return message.channel.send(`${message.author} has been nuked!`);
