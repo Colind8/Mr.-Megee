54d66cf8de32844933b42487ba158732b06fbcb5
Colind8 <colind8.awesome@gmail.com>
2023-04-17 16:32:47 -0500

    Fix errors with nuke update
    - Switch signs from <= to <
    - Make pn a var instead of const

--- commands/nuke.js
+++ commands/nuke.js
@@ -9,7 +9,7 @@ module.exports = {
 	async execute(message) {
         const args = message.content.slice(prefix.length).trim().split(" ");
 	    const command = args.shift().toLowerCase();
-        const pn = args[1] // pn = "ping number" or "number of pings"
+        var pn = args[1] // pn = "ping number" or "number of pings"
         
         if (args[1]) { 
             if(isNaN(args[1])) { // is [# of pings] argument NaN, which also deals with empty string
@@ -29,13 +29,13 @@ module.exports = {
                 return message.reply('Who?')
             }
 
-            for (i = 0; i =< pn-1; i++) {
+            for (i = 0; i < pn-1; i++) {
                 message.channel.send(`${user} is being nuked!`);
             }
             return message.channel.send(`${user} has been nuked!`);
         }
 
-        for (i = 0; i =< pn-1; i++) {
+        for (i = 0; i < pn-1; i++) {
                 message.channel.send(`${message.author} is being nuked!`);
             }
         return message.channel.send(`${message.author} has been nuked!`);
